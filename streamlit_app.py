436496f0bc4c4ced8027b45348b1326150508032
manuhanono <101122859+manuhanono@users.noreply.github.com>
2024-06-20 19:24:34 +0000

    p1

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,10 +5,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_squared_error, r2_score
 import altair as alt
-import time
-import zipfile
-import seaborn as sns
-import matplotlib.pyplot as plt
+
 
 # Page title
 st.set_page_config(page_title='ML Model Building', page_icon='🤖')
